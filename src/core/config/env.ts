2b0ea0387c58fff4cf5ee5537423db647c83c619
MGarzon TouchTo <mgarzon@touchto.io>
2023-11-14 15:32:29 -0500

    fix env variables, adds deploy workflow

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -1,6 +1,6 @@
 import dotenv = require('dotenv');
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-export const DATABASE_NAME = process.env.DATABASE_NAME;
-export const PORT = process.env.PORT;
-export const NODE_ENV = process.env.NODE_ENV;
+export const DATABASE_NAME = process.env.DATABASE_NAME || 'test';
+export const PORT = process.env.PORT || 3000;
+export const NODE_ENV = process.env.NODE_ENV || 'development';
